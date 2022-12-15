8484196bbab8b8f9f631ea181e9711b815cd3b7e
Vcyprus32@mail.ru <Vcyprus32@mail.ru>
2022-12-15 21:40:35 +0200

    Добавьте файлы проекта.

---
+++ MAss1/Program.cs
@@ -0,0 +1,27 @@
+﻿// известно число жителей, проживающих в каждом доме улицы. Нумерация домов проведена подряд. Две стороны чет и нечет. На какой стороне больше живет людей
+
+int[] House = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+int sumResidentsRight = 0;
+int sumResidentsLeft = 0;
+
+for (int i = 0; i < House.Length; i++) // число жителей приравнивали к нумерации дома 1=1 2=2 3=3 4=4 и т.д.
+{
+    if (House[i] % 2 == 0)
+    {
+        sumResidentsRight += House[i];
+    }
+
+    if (House[i] % 2 != 0)
+    {
+        sumResidentsLeft += House[i];
+    }
+}
+
+if (sumResidentsRight > sumResidentsLeft)
+{
+    Console.WriteLine($"С правой стороны живет больше людей, всего = {sumResidentsRight}");
+}
+else
+{
+    Console.WriteLine($"С левой стороны живет больше людей, всег = {sumResidentsLeft}");
+}
